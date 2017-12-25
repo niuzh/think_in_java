5fb06bd6323a05a8d39c30567acf7ab011039174
niuzhihuan <zhihuan.niu@funi.com>
2017-12-25 22:12:57 +0800

    02

---
+++ src/ch02/HelloDate.java
@@ -0,0 +1,12 @@
+package ch02;
+//引用程序使用到的类，程序默认倒入java.lang类库
+import java.util.Date;
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,it's :");
+		System.out.println(new Date());
+	}
+
+}
