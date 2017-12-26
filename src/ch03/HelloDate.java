1d48021060c61d3168500cae383f8ff3f43b3083
niuzhihuan <zhihuan.niu@funi.com>
2017-12-26 11:02:14 +0800

    c

---
+++ src/ch03/HelloDate.java
@@ -0,0 +1,19 @@
+package ch03;
+
+import java.util.*;
+import static java.lang.System.out;
+/**
+ * TODO
+ * @author niuzhihuan 2017-12-26
+ */
+public class HelloDate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		out.println("hello,it's:");
+		out.print(new Date());
+	}
+
+}
