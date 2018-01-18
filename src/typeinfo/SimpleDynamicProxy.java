1edc413a508dd42c8887f91d7ccb1bbb62ff83c6
niuzhihuan <zhihuan.niu@funi.com>
2018-01-18 09:59:32 +0800

    14

--- src/typeinfo/SimpleDynamicProxy.java
+++ src/typeinfo/SimpleDynamicProxy.java
@@ -2,17 +2,32 @@ package typeinfo;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import com.sun.org.apache.regexp.internal.recompile;
 
 class DynamicProxyHandler implements InvocationHandler{
-private Object proxied;
+	private Object proxied;
+
+	public DynamicProxyHandler(Interface interface1) {
+		this.proxied=interface1;
+	}
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("*** proxy:"+proxy.getClass()+".method: "+method+", args: "+args);
+		return method.invoke(proxied, args);
 	}
 }
 public class SimpleDynamicProxy{
-	
-
+	public static void consumer(Interface interface1) {
+		interface1.doSomething();
+		interface1.somethingElse("bononbo");
+	}
+	public static void main(String[] args) {
+		RealObject object=new RealObject();
+		consumer(object);
+		Interface proxy=(Interface)Proxy.newProxyInstance(SimpleDynamicProxy.class.getClassLoader(), new Class[]{Interface.class}, new DynamicProxyHandler(object));
+		consumer(proxy);
+	}
 }
---
+++ src/typeinfo/SnowRemovalRobot.java
@@ -0,0 +1,39 @@
+package typeinfo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SnowRemovalRobot implements Robot{
+	private String name;
+	public SnowRemovalRobot(String name) {
+		this.name=name;
+	}
+	@Override
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public String model() {
+		return "snowBot series 11";
+	}
+
+	@Override
+	public List<Operation> operations() {
+		return Arrays.asList(new Operation() {
+			
+			@Override
+			public String description() {
+				return name+" can shovel snow";
+			}
+			
+			@Override
+			public void command() {
+				System.out.println(name+" shoveling snow");
+			}
+		});
+	}
+	public static void main(String[] args) {
+		Robot.Test.test(new SnowRemovalRobot("slusher"));
+	}
+}
