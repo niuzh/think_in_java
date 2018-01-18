ab1ab1cb1a4d691c777d2a2410e977a24380d0e9
niuzhihuan <zhihuan.niu@funi.com>
2018-01-18 22:17:26 +0800

    1

---
+++ src/generics/Cappuccion.java
@@ -0,0 +1,5 @@
+package generics;
+
+public class Cappuccion extends Coffee {
+
+}
---
+++ src/generics/CoffeeGenerator.java
@@ -0,0 +1,54 @@
+package generics;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class CoffeeGenerator implements Generator<Coffee>, Iterable<Coffee> {
+	private Class[] types={Latte.class,Mocha.class,Cappuccion.class,Americano.class,Breve.class};
+	private static Random rand=new Random(47);
+	public CoffeeGenerator() {
+	}
+	private int size=0;
+	public CoffeeGenerator(int sz){
+		this.size=sz;
+	}
+	@Override
+	public Iterator<Coffee> iterator() {
+		// TODO Auto-generated method stub
+		return new CoffeeIterator();
+	}
+
+	@Override
+	public Coffee next() {
+		try {
+			return (Coffee)
+					(types[rand.nextInt(types.length)].newInstance());
+		} catch (Exception e) {
+		}
+	}
+	
+	class CoffeeIterator implements Iterator<Coffee>{
+		int count=size;
+		public boolean hasNext() {
+			return count>0;
+		}
+		@Override
+		public Coffee next() {
+			// TODO Auto-generated method stub
+			return CoffeeGenerator.this.next();
+		}
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	};
+	public static void main(String[] args){
+		CoffeeGenerator generator=new CoffeeGenerator();
+		for (int i = 0; i < 5; i++) {
+			System.out.println(generator.next());
+		}
+		for (Coffee coffee : generator) {
+			System.out.println(coffee);
+		}
+	}
+}
