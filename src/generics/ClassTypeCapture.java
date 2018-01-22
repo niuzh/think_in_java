1f535defb24746e0f42dae382beff54a24f16796
niuzhihuan <zhihuan.niu@funi.com>
2018-01-22 15:38:21 +0800

    15

---
+++ src/generics/ClassTypeCapture.java
@@ -0,0 +1,23 @@
+package generics;
+
+class Building{}
+class House extends Building{}
+
+public class ClassTypeCapture<T> {
+	private Class<T> kind;
+	public ClassTypeCapture(Class<T> kind) {
+		this.kind=kind;
+	}
+	public boolean f(Object arg) {
+		return kind.isInstance(arg);
+	}
+	public static void main(String[] args) {
+		ClassTypeCapture<Building> ctt1=new ClassTypeCapture<>(Building.class);
+		System.out.println(new Building());
+		System.out.println(new House());
+		ClassTypeCapture<House> ctt2=new ClassTypeCapture<>(House.class);
+		System.out.println(ctt2.f(new Building()));
+		System.out.println(ctt2.f(new House()));
+	}
+
+}
